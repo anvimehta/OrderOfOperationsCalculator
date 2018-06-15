c12de9b142f997c448fcb76b853816140d79e3e4
anvimehta <mehta109@purdue.edu>
2018-06-15 16:27:11 +0530

    Implemented RecyclerView in ViewHistoryActivity and setting adapter to it

--- app/src/main/java/edu/purdue/mehta109/trialanderror/ViewHistoryActivity.java
+++ app/src/main/java/edu/purdue/mehta109/trialanderror/ViewHistoryActivity.java
@@ -2,14 +2,22 @@ package edu.purdue.mehta109.trialanderror;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class ViewHistoryActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycler_view_history)
+    protected RecyclerView mRecyclerViewHistory;
 
     private Realm realm;
 
@@ -17,6 +25,7 @@ public class ViewHistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_history);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -24,11 +33,17 @@ public class ViewHistoryActivity extends AppCompatActivity {
         fetchHistoryFromRealm();
     }
 
+    private void setHistoryListAdapter(List<EquationModel> equationModelList) {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager
+                .VERTICAL, false);
+        mRecyclerViewHistory.setLayoutManager(linearLayoutManager);
+        mRecyclerViewHistory.setAdapter(new HistoryListAdapter(equationModelList));
+    }
+
     private void fetchHistoryFromRealm() {
-        RealmResults<EquationModel> eqAndAns = realm.where(EquationModel.class).findAll();
-        for (int i = 0; i < eqAndAns.size(); i++) {
-            Log.v("output", ":" + eqAndAns.get(i).getEquation());
-            Log.v("answer", ":" + eqAndAns.get(i).getAnswer());
+        RealmResults<EquationModel> historyList = realm.where(EquationModel.class).findAll();
+        if (historyList != null && historyList.size() > 0) {
+            setHistoryListAdapter(historyList);
         }
     }
 
