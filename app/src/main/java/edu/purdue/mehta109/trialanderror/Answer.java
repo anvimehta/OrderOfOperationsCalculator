6725661554a9f948cc8c027d7a9602614fea6307
anvimehta <mehta109@purdue.edu>
2018-06-18 11:15:37 +0530

    removed useless files

--- app/src/main/java/edu/purdue/mehta109/trialanderror/Answer.java
+++
@@ -1,7 +0,0 @@
-package edu.purdue.mehta109.trialanderror;
-
-import io.realm.RealmObject;
-
-public class Answer extends RealmObject{
-    public String answer;
-}
--- app/src/main/java/edu/purdue/mehta109/trialanderror/Equation.java
+++
@@ -1,7 +0,0 @@
-package edu.purdue.mehta109.trialanderror;
-
-import io.realm.RealmObject;
-
-public class Equation extends RealmObject{
-    public String eq;
-}
